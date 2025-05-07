320242c8fe40bfc7c52b0f69f9e48aafa4fe0c41
Tarkan Gur <tarkangur13@gmail.com>
2025-05-07 23:44:51 +0300

    null kisimlara blank de eklendi.

--- users/models.py
+++ users/models.py
@@ -6,12 +6,12 @@ class Users(AbstractUser):
     street = models.CharField(max_length=255)
     suite = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-    zipcode = models.IntegerField(null=True)
-    lat = models.FloatField(null=True)
-    lng = models.FloatField(null=True)
-    phone = models.CharField(max_length=20, default="")
-    website = models.URLField(max_length=255, null=True)
-    company_name = models.CharField(max_length=255)
+    zipcode = models.IntegerField(null=True, blank=True)
+    lat = models.FloatField(null=True, blank=True)
+    lng = models.FloatField(null=True, blank=True)
+    phone = models.CharField(max_length=20, default="", blank=True)
+    website = models.URLField(max_length=255, null=True, blank=True)
+    company_name = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.username
